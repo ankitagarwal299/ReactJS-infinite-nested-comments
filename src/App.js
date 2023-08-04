a7d92753079c349348eaa7f560ec32313323f3bb
ankit agarwal <ankit.agarwal299@gmail.com>
2023-08-04 22:34:35 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import useNode from "./hooks/useNode";
 import "./styles.css";
 
 //ReactJS-infinite-nested-comments
+//https://www.youtube.com/watch?v=EGXnsGenlCg&t=568s
+//https://github.com/xplodivity/infinite-nested-comments
+
 const comments = {
   id: 1,
   items: [],
